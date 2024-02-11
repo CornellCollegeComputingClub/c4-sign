2640ac2ac21877504a0fda868473b6ef9b9f22fe
Mark Smith <totallynotmark6@gmail.com>
2024-02-11 17:43:10 -0600

    add lib folder

---
+++ c4_sign/lib/liquidcrystal/simulator.py
@@ -0,0 +1,9 @@
+# from c4_sign.lib.liquidcrystal.base import LiquidCrystalBase
+from base import LiquidCrystalBase
+
+
+class SimulatedLCD(LiquidCrystalBase):
+    pass
+
+if __name__ == "__main__":
+    s = SimulatedLCD(2, 16)
