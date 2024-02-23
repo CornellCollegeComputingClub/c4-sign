353d545d45a14c4af1e4dd0e5cb661ed8242e7ce
Mark Smith <totallynotmark6@gmail.com>
2024-02-23 15:49:21 -0600

    see if the physical thing works...

--- c4_sign/lib/screen/matrix.py
+++ c4_sign/lib/screen/matrix.py
@@ -1,5 +1,16 @@
+from c4_sign.lib.canvas import Canvas
 from c4_sign.lib.screen.base import ScreenBase
-
+import neopixel
+import board
 
 class MatrixScreen(ScreenBase):
-    pass
+    def __init__(self):
+        self.__pixels = neopixel.NeoPixel(board.D18, 32*32, brightness=0.05, auto_write=False)
+
+    def update_display(self, canvas: Canvas):
+        self.__pixels[::1] = canvas.tolist()
+        self.__pixels.show()
+    
+    def update_lcd(self, text):
+        pass
+        # return super().update_lcd(text)
