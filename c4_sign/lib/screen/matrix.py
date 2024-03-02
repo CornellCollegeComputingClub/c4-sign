d5b128120d9d00ba46c92af6652aa8fe517f8035
Mac-Coleman <williamcormaccoleman@gmail.com>
2024-03-02 00:17:59 -0600

    Untested addressing code

--- c4_sign/lib/screen/matrix.py
+++ c4_sign/lib/screen/matrix.py
@@ -1,5 +1,6 @@
 import board
 import neopixel
+import numpy
 
 from c4_sign.lib.canvas import Canvas
 from c4_sign.lib.screen.base import ScreenBase
@@ -13,11 +14,43 @@ class MatrixScreen(ScreenBase):
         self.__cached_text = " " * 32
         self.loading_screen()
 
+        # Generating address table...
+        quadrant_one = []
+
+        # Generate the zigzag for one quadrant.
+        for i in range(0, 8):
+            row1, row2 = [], []
+            for j in range(15, -1, -1):
+                row1.append(i*32 + j)
+
+            for j in range(0, 16):
+                row2.append((i*32) + 16 + j)
+
+            quadrant_one.append(row1)
+            quadrant_one.append(row2)
+
+        # Add constant offset to all four quadrants.
+        from copy import deepcopy
+        top_right = quadrant_one
+        top_left = [[y + 256 for y in x] for x in deepcopy(quadrant_one)]
+        bot_right = [[y + 512 for y in x] for x in deepcopy(quadrant_one)]
+        bot_left = [[y + 768 for y in x] for x in deepcopy(quadrant_one)]
+
+        for i, x in enumerate(top_left):
+            x.extend(top_right[i])
+
+        for i, x in enumerate(bot_left):
+            x.extend(bot_right[i])
+
+        top_left.extend(bot_left)
+
+        self.__address_table = numpy.argsort(numpy.array(top_left).reshape(1024))
+        # 1024-long list of addresses
+
     def update_display(self, canvas: Canvas):
         # for i in range(32*32):
         #     self.__pixels[i] = canvas[i]
-        x = lambda i: self.__pixels.__setitem__(i, canvas[i])
-        [x(i) for i in range(32 * 32)]
+        self.__pixels[:] = canvas.data.reshape((1024, 3))[self.__address_table]
         self.__pixels.show()
 
     def update_lcd(self, text):
