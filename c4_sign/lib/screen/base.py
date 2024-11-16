1eaae6e610ad0d50a0ca24a402b4e9ade460bf05
Luna <itsactuallyluna9@gmail.com>
2024-11-16 00:17:02 -0600

    add visuals to loading

--- c4_sign/lib/screen/base.py
+++ c4_sign/lib/screen/base.py
@@ -23,13 +23,21 @@ class ScreenBase(ABC):
 
     def loading_screen(self):
         from time import sleep
+        from c4_sign.lib.graphics import draw_text
+        from c4_sign.consts import FONT_PICO, COLOR_WHITE
 
         sleep(1)  # wait for things to finish setting up so we can draw
         self.update_lcd("Loading...".ljust(32))
         c = Canvas()
+        draw_text(c, FONT_PICO, 1, 6, COLOR_WHITE, ">Load")
         self.update_display(c)
 
     def loading_cb(self, text):
+        from c4_sign.lib.graphics import draw_text
+        from c4_sign.consts import FONT_PICO, COLOR_GRAY, COLOR_WHITE
+
         self.update_lcd("Loading...".ljust(16) + text.ljust(16))
         c = Canvas()
+        draw_text(c, FONT_PICO, 1, 6, COLOR_WHITE, ">Load")
+        draw_text(c, FONT_PICO, 1, 12, COLOR_GRAY, text)
         self.update_display(c)
