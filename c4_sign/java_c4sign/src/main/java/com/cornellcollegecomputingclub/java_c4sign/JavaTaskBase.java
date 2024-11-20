0df91cb48a5af5c1d57d9012ef0c052337508474
Mac-Coleman <williamcormaccoleman@gmail.com>
2024-11-20 00:27:26 -0600

    Added Constants.java

--- c4_sign/java_c4sign/src/main/java/com/cornellcollegecomputingclub/java_c4sign/JavaTaskBase.java
+++ c4_sign/java_c4sign/src/main/java/com/cornellcollegecomputingclub/java_c4sign/JavaTaskBase.java
@@ -1,5 +1,7 @@
 package com.cornellcollegecomputingclub.java_c4sign;
 
+import com.cornellcollegecomputingclub.java_c4sign.Constants;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
@@ -9,11 +11,12 @@ public abstract class JavaTaskBase {
     private String title;
     private String artist;
     private byte[][][] canvas;
+    public static boolean ignored = false;
 
     public JavaTaskBase() {
         this.title = "Unknown";
         this.artist = "Unknown";
-        this.canvas = new byte[32][32][3];
+        this.canvas = new byte[Constants.SCREEN_WIDTH][Constants.SCREEN_HEIGHT][Constants.SCREEN_COLORS];
     }
 
     public void setTitle(String newTitle) {
@@ -35,17 +38,17 @@ public abstract class JavaTaskBase {
     public abstract boolean drawFrame(byte[][][] canvas, double timeDelta);
 
     public boolean draw(double timeDelta) {
-        this.canvas = new byte[32][32][3];
+        this.canvas = new byte[Constants.SCREEN_WIDTH][Constants.SCREEN_HEIGHT][Constants.SCREEN_COLORS];
         boolean finished = this.drawFrame(this.canvas, timeDelta);
         return finished;
     }
 
     public byte[] retrieveCanvas() {
-        ByteBuffer intBuffer = ByteBuffer.allocate(3*32*32);
+        ByteBuffer intBuffer = ByteBuffer.allocate(Constants.SCREEN_COLORS*Constants.SCREEN_WIDTH*Constants.SCREEN_HEIGHT);
         intBuffer.order(ByteOrder.LITTLE_ENDIAN);
 
-        for (int i = 0; i < 32; i++) {
-            for (int j = 0; j < 32; j++) {
+        for (int i = 0; i < Constants.SCREEN_WIDTH; i++) {
+            for (int j = 0; j < Constants.SCREEN_HEIGHT; j++) {
                 intBuffer.put(this.canvas[i][j]);
             }
         }
@@ -54,9 +57,9 @@ public abstract class JavaTaskBase {
     }
 
     public String getLcdText() {
-        String line1 = StringUtils.center(this.title, 16).substring(0, 16);
+        String line1 = StringUtils.center(this.title, Constants.LCD_WIDTH).substring(0, Constants.LCD_WIDTH);
         String line2 = "By: ";
-        line2 = line2.concat(StringUtils.center(this.artist, 16)).substring(0, 16);
+        line2 = line2.concat(StringUtils.center(this.artist, Constants.LCD_WIDTH)).substring(0, Constants.LCD_WIDTH);
         return line1.concat(line2);
     }
 }
