1d7b1242c917c414d0d8e95494c6064ce81cba65
Mac-Coleman <williamcormaccoleman@gmail.com>
2024-11-23 02:50:21 -0600

    Delete java-related shell scripts (replaced with Python)

--- bin/start_java_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-cd "$(dirname "$0")"
-
-echo "Starting Java server!"
-java -cp ~/.m2/repository/org/py4j/py4j/0.10.9.7/py4j-0.10.9.7.jar:../c4_sign/java_c4sign/target/java_c4sign-1.0-SNAPSHOT.jar com.cornellcollegecomputingclub.java_c4sign.App
