77fa6fb78626f214c8e095c7c5bcc7f8d347b945
Mac-Coleman <williamcormaccoleman@gmail.com>
2025-02-12 16:43:15 -0600

    Added rainbow colors to Lissajous to demonstrate Java BufferedImage uses

--- c4_sign/base_task.py
+++ c4_sign/base_task.py
@@ -327,7 +327,7 @@ class JavaTask(ScreenTask):
         canvas.data = numpy.array(bytearray(taskresult.getCanvas())).reshape(32, 32, 3)
         return status
     
-    def get_lcd_text(self) -> str:
-        pass
+    # def get_lcd_text(self) -> str:
+        # pass
         # TODO: Fix StringUtils not being in classpath.
         # return self.__java_task_instance.getLcdText()
--- c4_sign/java_c4sign/src/main/java/com/cornellcollegecomputingclub/c4sign_tasks/Lissajous.java
+++ c4_sign/java_c4sign/src/main/java/com/cornellcollegecomputingclub/c4sign_tasks/Lissajous.java
@@ -2,7 +2,7 @@ package com.cornellcollegecomputingclub.c4sign_tasks;
 
 import java.util.Random;
 import java.lang.Math;
-import java.awt.geom.Path2D;
+import java.awt.geom.Line2D;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.RenderingHints;
@@ -41,32 +41,33 @@ public class Lissajous extends JavaTaskBase{
 
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphics.setStroke(new BasicStroke(1.5f));
-        graphics.setColor(Color.WHITE);
-
-        Path2D.Double path = new Path2D.Double();
 
         double firstY = 0;
         double firstX = 0;
+
+        double lastY = 0;
+        double lastX = 0;
 
         for (int i=0; i < 64; i++) {
             double t = ((double) i / 64.0) * 2 * this.period; // This is wrong but I am LAZY
             
             double newY = Math.sin(this.freqA * t + this.phase) * 15.0 + 16.0;
             double newX = Math.sin(this.freqB * t) * 15.0 + 16.0;
-            
 
             if (i == 0) {
-                path.moveTo(newX, newY);
                 firstX = newX;
                 firstY = newY;
             } else {
-                path.lineTo(newX, newY);
+                graphics.setColor(Color.getHSBColor(2 * (float) i / 64.0f, 1.0f, 1.0f));
+                graphics.draw(new Line2D.Double(lastX, lastY, newX, newY));
             }
+
+            lastX = newX;
+            lastY = newY;
         }
 
-        path.lineTo(firstX, firstY);
-        
-        graphics.draw(path);
+        graphics.setColor(Color.getHSBColor(1.0f, 1.0f, 1.0f));
+        graphics.draw(new Line2D.Double(lastX, lastY, firstX, firstY));
 
         this.phase += deltaTime * 2.0;
 
