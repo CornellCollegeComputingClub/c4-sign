7a5f42234f826ed75465cb32b5c4f0be65d4daf3
Mac-Coleman <williamcormaccoleman@gmail.com>
2026-08-19 14:06:16 -0500

    Silenced command in stop_sign.sh

--- bin/stop_sign.sh
+++ bin/stop_sign.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/kill $MAINPID
-sudo -H /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "REMOVE" "POWER"
+sudo -H /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "REMOVE" "POWER" > /dev/null
