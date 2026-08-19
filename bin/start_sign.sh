40ecea9c1ac28968113465b705c8f5f3709e9315
Mac-Coleman <williamcormaccoleman@gmail.com>
2026-08-19 13:44:44 -0500

    Fixed while loop formatting, silenced status message commands

--- bin/start_sign.sh
+++ bin/start_sign.sh
@@ -5,14 +5,15 @@ set -o errexit
 
 cd /home/c4/c4-sign
 
-sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "NETWORK" "TARGET"
+sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "NETWORK" "TARGET" > /dev/null
 
-while [["$(systemctl is-active network.target)" != 'active']] do
+while [["$(systemctl is-active network.target)" != 'active']]
+do
     echo 'Waiting for network.'
-    sleep 0.2
+    sleep '0.2'
 done
 
-sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "INTERNET" "CHECK"
+sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "INTERNET" "CHECK" > /dev/null
 
 # do we have internet?
 function checkInternet {
@@ -24,18 +25,18 @@ while ! checkInternet; do
     sleep 5
 done
 
-sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "GIT" "PULL"
+sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "GIT" "PULL" > /dev/null
 
 git pull
 
 # sudo python3 -m pip install -e .
 # this takes a while to run. TODO: FIX.
 
-sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "JAVA" "COMPILE"
+sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "JAVA" "COMPILE" > /dev/null
 
 /home/c4/c4-sign/venv/bin/python3 tools/compile_java_project.py
 
-sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "STARTING" "SIGN"
+sudo /home/c4/c4-sign/venv/bin/python3 /home/c4/c4-sign/bin/message.py "STARTING" "SIGN" > /dev/null
 
 sudo /home/c4/c4-sign/venv/bin/python3 -m c4_sign
 
